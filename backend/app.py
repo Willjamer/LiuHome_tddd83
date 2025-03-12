b05ffef0f360318501718a2823ad688d064f9954
Christoffer Roempke <christoffer@Henriks-MacBook-Pro.local>
2025-03-12 10:55:08 +0100

    Glömde methods

--- backend/app.py
+++ backend/app.py
@@ -13,34 +13,34 @@ CORS(app, origins=["http://localhost:3000"])  # Allow requests from the frontend
 def home():
     return jsonify({"message": "Flask Backend Running"})
 
-@app.route("/api/add-appartment")
+@app.route("/api/add-appartment", methods=['POST'])
 def add_appartment():
     json_data = request.get_json()
     return handler.handle_json_add(json_data)
 
-@app.route("/api/get-appartment")
+@app.route("/api/get-appartment", methods=['GET'])
 def get_appartment():
     return handler.get_appartment
 
-@app.route("/api/get-user-profile")
+@app.route("/api/get-user-profile", methods=['GET'])
 def get_user_profile():
     return handler.get_user_profile
 
-@app.route("/api/get-user")
+@app.route("/api/get-user", methods=['GET'])
 def get_user():
     return handler.get_user
 
-@app.route("/api/get-listing")
+@app.route("/api/get-listing", methods=['GET'])
 def get_listing():
     json_data = request.get_json()
     return handler.get_Listing(json_data)
 
-@app.route("/api/remove-item")
+@app.route("/api/remove-item", methods=['POST'])
 def remove_item():
     json_data = request.get_json()
     return handler.remove_item(json_data)
 
-@app.route("/api/edit-item")
+@app.route("/api/edit-item", methods=['POST'])
 def edit_item():
     json_data = request.get_json()
     return handler.edit_item(json_data)
