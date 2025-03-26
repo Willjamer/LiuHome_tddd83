b23a5eaccf85f34f9961d2a83e54935954a8b86f
Jonatan Billger <jonte.billger@gmail.com>
2025-03-26 23:42:19 +0100

    added filtering functions

--- backend/database.py
+++ backend/database.py
@@ -9,26 +9,31 @@ import characters
 db = get_db()
 
 class Apartment(db.Model):
-    apartment_id   = db.Column(db.Integer, primary_key = True)
-    user_id        = db.Column(db.Integer, db.ForeignKey("user.sso_id"), unique = True, nullable = False)
-    title          = db.Column(db.String, nullable = False)
-    description    = db.Column(db.String, nullable = True)
-    location       = db.Column(db.String, nullable = False)
-    rent_amount    = db.Column(db.Integer, nullable = False)
-    is_available   = db.Column(db.Boolean, nullable = False, default = True)
-    available_from = db.Column(db.Date, nullable = True)
+    apartment_id    = db.Column(db.Integer, primary_key = True)
+    user_id         = db.Column(db.Integer, db.ForeignKey("user.sso_id"), unique = True, nullable = False)
+    title           = db.Column(db.String, nullable = False)
+    description     = db.Column(db.String, nullable = True)
+    size            = db.Column(db.Float, nullable = False)
+    number_of_rooms = db.Column(db.Integer, primary_key = True)
+    location        = db.Column(db.String, nullable = False)
+    rent_amount     = db.Column(db.Integer, nullable = False)
+    is_available    = db.Column(db.Boolean, nullable = False, default = True)
+    available_from  = db.Column(db.Date, nullable = True)
     # Något här om images, vet ej än hur
 
+    all_locations: list(str) = ["Ryd", "Valla", "Irrblosset", "T1", "Lambohov", "Gottfridsberg"]
+
     def __repr__(self):
-        return f"<Apartment {self.apartment_id}: {self.title}: {self.description}: {self.location}: {self.rent_amount}: {self.available_from}>"
+        return f"<Apartment {self.apartment_id}: {self.title}: {self.description}: {self.size}: {self.number_of_rooms}: {self.location}: {self.rent_amount}: {self.available_from}>"
 
     def serialize(self):
         return {
             "apartment_id" : self.apartment_id,
             "user_id" : self.user_id,
-            "review_id" : self.review_id,
             "title" : self.title,
             "description": self.description,
+            "size": self.size,
+            "number_of_rooms": self.number_of_rooms,
             "location": self.location,
             "rent_amount": self.rent_amount,
             "is_available": self.is_available,
@@ -131,6 +136,7 @@ def generate_unique_id(determinator):
 
     unique_strings = set("".join(p) for p in itertools.permutations(characters, 3))
 
+
 def get_all_available_apartments():
     apartments = Apartment.query.filter_by(is_available=True).all()
     if apartments: 
@@ -138,18 +144,101 @@ def get_all_available_apartments():
 
     return jsonify({'message': 'No available apartments', 'Apartments': []}) 
 
+
+def db_filter_by_rent(arrange):
+    
+    order = Apartment.rent_amount if arrange == "asc" else Apartment.rent_amount.desc()
+    apartments = Apartment.query.order_by(order).all()
+
+    if apartments:
+        return jsonify({'Apartments': [apartment.serialize() for apartment in apartments]})
+    return jsonify({'message': 'No apartments match your preferences', 'Apartments': []})
+
+
+def db_filter_by_location(locations):
+    apartments = Apartment.query.filter(Apartment.location.in_(locations)).all()    
+
+    if apartments:
+        return jsonify({'Apartments': [apartment.serialize() for apartment in apartments]})
+    return jsonify({'message': 'No apartments match your preferences', 'Apartments': []})
+
+
+def db_filter_by_size(min, max):
+    min = min if min else 0.0
+    max = max if max else 1000
+
+    apartments = Apartment.query.filter(Apartment.size >= min, Apartment.size <= max).all()
+
+    if apartments:
+        return jsonify({'Apartments': [apartment.serialize() for apartment in apartments]})
+    return jsonify({'message': 'No apartments match your preferences', 'Apartments': []})
+
+
+def db_filter_by_rooms(min, max):
+    min = min if min else 0
+    max = max if max else 15
+
+    apartments = Apartment.query.filter(Apartment.number_of_rooms >= min, Apartment.number_of_rooms <= max).all()
+
+    if apartments:
+        return jsonify({'Apartments': [apartment.serialize() for apartment in apartments]})
+    return jsonify({'message': 'No apartments match your preferences', 'Apartments': []})
+
+
+def db_sort_apartments(apartments, sort_factor, asc):
+    match sort_factor:
+        case "rent_amount":
+            order = apartments.rent_amount
+        case "size":
+            order = apartments.size
+
+    if not asc:
+        order = order.desc()
+
+    return apartments.order_by(order).all()
+
+
+def db_filtering(rent_interval, size_interval, room_interval, locations, sort_factor, asc):
+    
+    min_rent, max_rent = rent_interval[0], rent_interval[1]
+    min_size, max_size = size_interval[0], size_interval[1]
+    min_rooms, max_rooms = room_interval[0], room_interval[1]
+
+    if locations is None:
+        locations = Apartment.all_locations
+
+    apartments = Apartment.query.filter(Apartment.rent_amount >= min_rent, 
+                                 Apartment.rent_amount <= max_rent, 
+                                 Apartment.size >= min_size, 
+                                 Apartment.size <= max_size, 
+                                 Apartment.number_of_rooms >= min_rooms,
+                                 Apartment.number_of_rooms <= max_rooms,
+                                 Apartment.location.in_(locations))
+
+    if sort_factor:
+        apartments = db_sort_apartments(apartments, sort_factor, asc)
+    
+    apartments_list = apartments if isinstance(apartments, list) else apartments.all()
+
+    if apartments_list:
+        return jsonify({'Apartments': [apartment.serialize() for apartment in apartments_list]})
+    return jsonify({'message': 'No apartments match your preferences', 'Apartments': []})
+
+    
 def db_get_specific_apartment(this_apartment_id):
     this_apartment = Apartment.query.get(this_apartment_id)
 
     return jsonify({'apartment' : this_apartment.serialize()})
 
-def db_add_apartment(user_id, title, description, location, rent_amount, available_from):
+def db_add_apartment(user_id, title, description, size, number_of_rooms, location, rent_amount, available_from):
     try:
         new_apartment = Apartment(
             apartment_id = 1000, # Temporary
             user_id = user_id,
             title = title,
             description= description,
+            size = size,
+            number_of_rooms = number_of_rooms,
             location = location,
             rent_amount = rent_amount,
             is_available = True,
